420142455a042b4547bccbd96db4a309ab7560d4
aminhasanov <aminhesenov21@gmail.com>
2019-11-28 11:56:30 +0400

    news edited

---
+++ intranet.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../Service/auth.service';
+import {AlertifyjsService} from '../Service/alertifyjs.service';
+import {News} from '../Models/news';
+
+
+@Component({
+  selector: 'app-intranet',
+  templateUrl: './intranet.component.html',
+  styleUrls: ['./intranet.component.css']
+})
+export class IntranetComponent implements OnInit {
+  news: News[];
+  constructor(private authService: AuthService, private alertify: AlertifyjsService) { }
+  ngOnInit() {
+    this.getNews();
+  }
+  getNews() {
+    console.log('hethdty');
+    this.authService.getNews().subscribe((result: News[]) => {
+      this.news = result;
+    }, error => {
+      this.alertify.error('Daxili Server xətası');
+    });
+    console.log(this.news);
+  }
+}
